6f1909fc62304875d1bf41ff708c808bd3eceeae
Md Sayed Al Fardosh <alfardosh@gmail.com>
2026-05-26 16:42:56 +0600

    Fix routing using HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { HashRouter } from 'react-router-dom';
+
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </HelmetProvider>
   </StrictMode>,
 );
